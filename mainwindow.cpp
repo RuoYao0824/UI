9dbbe990f1f2e6035d137693c0b5ff6be35124db
RuoYao0824 <ruoyaofang@gmail.com>
2024-05-21 12:54:30 +0800

    quit button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,30 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QDebug>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QObject::connect(ui->Quit_btn, &QPushButton::clicked, this, &MainWindow::quit);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::quit()
+{
+    QMessageBox::StandardButton reply;
+    reply = QMessageBox::question(this, "Qt with SuperChicken", "Are you sure to close the window?", QMessageBox::Yes | QMessageBox::No);
+    if(reply == QMessageBox::Yes){
+        qDebug() << "quit" ; //remember to save file in the future
+        QApplication::quit();
+    }
+    
+}
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void quit();
+
 private:
     Ui::MainWindow *ui;
 };
